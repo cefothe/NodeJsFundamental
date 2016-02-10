628ac9712315f658a846451f731d37f69d435398
Stefan Angelov <stefanangelov@deltasource.eu>
2016-02-10 13:49:58 +0200

    Simple routing

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response){
-	response.writeHead(200,{"Content-type":"text/html"});
-	response.end("hello <strong>world</strong>");
+	if(request.url === '/'){
+		response.writeHead(200,{"Content-type":"text/html"});
+		response.end("hello <strong>world</strong>");
+	}else if (request.url === '/goodbye'){
+		response.writeHead(200,{"Content-type":"text/html"});
+		response.end("goodbye <strong>world</strong>");
+	}
 });
 
 server.listen(3000);
